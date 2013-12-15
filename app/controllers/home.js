16db485f869e78df1ae3e93d92767d0133f02134
Rodrigo <rodrigo@rodrigo-note.(none)>
2013-12-15 18:32:09 -0200

    novos arquvios

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,55 @@
 var mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+Article = mongoose.model('Article');
 
 exports.index = function(req, res){
-  Article.find(function(err, articles){
-    if(err) throw new Error(err);
-    res.render('home/index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
+	Article.find(function(err, articles){
+		if(err) throw new Error(err);
+		res.render('home/index', {
+			title: 'Generator-Express MVC',
+			articles: articles
+		});
+	});
 };
+
+exports.login =  function(req, res) {
+	var email = req.body.email;
+	var nome = req.body.nome;
+	if(email && nome) {
+		var usuario = new Usuario(nome, email);
+		req.session.usuario = usuario;
+		res.redirect('/contatos');
+	} else {
+		res.redirect('/');
+	}
+};
+
+exports.logout = function(req, res) {
+ 			req.session.destroy();
+ 			res.redirect('/');
+ 		};
+
+// module.exports = function(app) {
+// 	var Usuario = app.models.usuario;
+// 	var HomeController = {
+// 		index: function(req, res) {
+
+// 			res.render('home/index');
+// 		},
+// 		login: function(req, res) {
+// 			var email = req.body.email;
+// 			var nome = req.body.nome;
+// 			if(email && nome) {
+// 				var usuario = new Usuario(nome, email);
+// 				req.session.usuario = usuario;
+// 				res.redirect('/contatos');
+// 			} else {
+// 				res.redirect('/');
+// 			}
+// 		},
+// 		logout: function(req, res) {
+// 			req.session.destroy();
+// 			res.redirect('/');
+// 		}
+// 	};
+// 	return HomeController;
+// };
--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,7 @@ module.exports = function(app){
 	//home route
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
+	app.post('/entrar', home.login);
+	app.get('/sair', home.logout);
 
 };
